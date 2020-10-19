ff55501ac956469fdb4dcd8d85f9c9a22170cbac
aditi <aditi@DESKTOP-343N0FS>
2020-10-19 22:17:34 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan15/logika/IF110119016Latihan15Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119016.latihan15.logika;
+
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menampilkan logika
+ */
+public class IF110119016Latihan15Logika {
+
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a  = " + a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
